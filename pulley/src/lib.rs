69a50197eef6677e78d0c126e7bb6da2bf50f5b6
Tyler Neely <t@jujit.su>
2018-03-01 00:17:23 +0100

    add simple thread scheduler

---
+++ pulley/src/lib.rs
@@ -0,0 +1,175 @@
+extern crate rand;
+
+use rand::{Rng, StdRng, SeedableRng};
+use std::cell::RefCell;
+use std::sync::mpsc::{SyncSender, Receiver, sync_channel};
+
+thread_local! {
+    pub static HANDLE: RefCell<Option<Handle>> = RefCell::new(None);
+}
+
+#[macro_export]
+macro_rules! step {
+    ($label:expr) => {
+        $crate::HANDLE.with(|h| if let Some(ref handle) = *h.borrow() {
+            handle.step();
+        } else {
+            panic!("step called outside of a scheduled context");
+        });
+    };
+    () => {
+        step!(format!("%s:%s", module_path!(), line!()));
+    }
+}
+
+pub type ScheduledFn<State> = fn(&State);
+
+#[derive(Debug, PartialEq)]
+enum SchedulerMessage {
+    Rendezvous,
+    Step,
+    Exit,
+}
+
+fn handle_pair() -> (SchedulerHandle, Handle) {
+    let (to_thread, from_sched) = sync_channel(0);
+    let (to_sched, from_thread) = sync_channel(0);
+
+    let sh = SchedulerHandle {
+        to_thread: to_thread,
+        from_thread: from_thread,
+    };
+
+    let h = Handle {
+        to_sched: to_sched,
+        from_sched: from_sched,
+    };
+
+    (sh, h)
+}
+
+struct SchedulerHandle {
+    to_thread: SyncSender<()>,
+    from_thread: Receiver<SchedulerMessage>,
+}
+
+pub struct Handle {
+    to_sched: SyncSender<SchedulerMessage>,
+    from_sched: Receiver<()>,
+}
+
+impl Handle {
+    fn bind(self) {
+        self.to_sched.send(SchedulerMessage::Rendezvous).unwrap();
+        self.from_sched.recv().unwrap();
+        HANDLE.with(|h| *h.borrow_mut() = Some(self));
+    }
+
+    pub fn step(&self) {
+        self.to_sched.send(SchedulerMessage::Step).unwrap();
+        self.from_sched.recv().unwrap();
+    }
+}
+
+impl Drop for Handle {
+    fn drop(&mut self) {
+        self.to_sched.send(SchedulerMessage::Exit).unwrap();
+    }
+}
+
+struct PtrHack<State>(*mut State);
+
+unsafe impl<State> Send for PtrHack<State> {}
+unsafe impl<State> Sync for PtrHack<State> {}
+
+impl<State> Clone for PtrHack<State> {
+    fn clone(&self) -> PtrHack<State> {
+        PtrHack(self.0)
+    }
+}
+
+pub struct Scheduler<State> {
+    targets: Vec<ScheduledFn<State>>,
+    initializer: fn() -> State,
+}
+
+impl<State> Scheduler<State>
+where
+    State: 'static,
+{
+    pub fn with_initializer(f: fn() -> State) -> Scheduler<State> {
+        Scheduler {
+            targets: vec![],
+            initializer: f,
+        }
+    }
+
+    pub fn add(&mut self, f: ScheduledFn<State>) {
+        self.targets.push(f);
+    }
+
+    pub fn explore(self) {
+        for i in 0..100_000 {
+            self.run_with_seed(i);
+        }
+    }
+
+    pub fn run_with_seed(&self, seed: usize) {
+        //println!("----- seeding with {}", seed);
+        let mut rng = StdRng::from_seed(&[seed]);
+
+        // initialize threads
+        let state = PtrHack(Box::into_raw(Box::new((self.initializer)())));
+        let mut sched_handles = vec![];
+        let mut threads = vec![];
+
+        let targets = self.targets.clone();
+        for target in targets {
+            let (sched_handle, handle) = handle_pair();
+            let state = state.clone();
+
+            let thread = std::thread::spawn(move || {
+                handle.bind();
+                unsafe {
+                    (target)(&*state.0);
+                }
+            });
+
+            assert_eq!(
+                sched_handle.from_thread.recv().unwrap(),
+                SchedulerMessage::Rendezvous
+            );
+
+            sched_handles.push(sched_handle);
+            threads.push(thread);
+        }
+
+        // pick a random one until all are done
+        while !sched_handles.is_empty() {
+            let choice: usize = rng.gen_range(0, sched_handles.len());
+            println!("choice: {}", choice);
+            let should_drop = {
+                let sh = &sched_handles[choice];
+                sh.to_thread.send(()).unwrap();
+                match sh.from_thread.recv().unwrap() {
+                    SchedulerMessage::Step => false,
+                    SchedulerMessage::Exit => true,
+                    SchedulerMessage::Rendezvous => {
+                        panic!("got Rendezvous while stepping thread")
+                    }
+                }
+            };
+            if should_drop {
+                sched_handles.remove(choice);
+                if let Err(_) = threads.remove(choice).join() {
+                    panic!("failed test using seed {}", seed);
+                }
+                //println!("ended thread {}", choice);
+            }
+        }
+
+        unsafe {
+            drop(Box::from_raw(state.0));
+        }
+    }
+}
---
+++ pulley/tests/complex_race.rs
@@ -0,0 +1,88 @@
+#[macro_use]
+extern crate crack;
+
+use std::cell::UnsafeCell;
+use std::sync::Mutex;
+
+fn t1(ss: &SharedState) {
+    for _ in 0..2 {
+        step!();
+        let _ = ss.mu.lock().unwrap();
+        unsafe {
+            step!();
+            *ss.x.get() = 1;
+            step!();
+            *ss.y.get() = 1;
+        }
+        step!();
+        // implicit unlock
+    }
+}
+
+fn t2(ss: &SharedState) {
+    for _ in 0..2 {
+        {
+            step!();
+            let _ = ss.mu.lock().unwrap();
+            unsafe {
+                step!();
+                *ss.x.get() = 0;
+            }
+            step!();
+            // implicit unlock
+        }
+        unsafe {
+            step!();
+            if *ss.x.get() > 0 {
+                step!();
+                *ss.y.get() += 1;
+                step!();
+                *ss.x.get() = 2;
+            }
+        }
+    }
+}
+
+fn t3(ss: &SharedState) {
+    for _ in 0..2 {
+        unsafe {
+            step!();
+            if *ss.x.get() > 1 {
+                step!();
+                if *ss.y.get() == 3 {
+                    panic!("ahhh wtf");
+                } else {
+                    step!();
+                    *ss.y.get() = 2;
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+struct SharedState {
+    mu: Mutex<()>,
+    x: UnsafeCell<usize>,
+    y: UnsafeCell<usize>,
+}
+
+#[test]
+fn complex_race() {
+    let l = || {
+        SharedState {
+            mu: Mutex::new(()),
+            x: UnsafeCell::new(0),
+            y: UnsafeCell::new(0),
+        }
+    };
+
+    let mut scheduler = crack::Scheduler::with_initializer(l);
+
+    scheduler.add(t1);
+    scheduler.add(t2);
+    scheduler.add(t3);
+
+    scheduler.run_with_seed(20852);
+    // scheduler.explore();
+}
